dec58ea0f88cfe21e8985f2cf09eff14faa5e158
Bas Leenknegt <basleenknegt@gmail.com>
2026-05-13 14:25:50 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Globalise Webapp
+
+Integrate design with search and document components.
+
+When adding a new package:
+ - add path to root tsconfig.json
+ - add dependency to consuming package.json
---
+++ packages/app/src/App.tsx
@@ -0,0 +1,14 @@
+import {Design} from "@globalise/design";
+import {Document} from "@globalise/document";
+import {Search} from "@globalise/search";
+
+export function App() {
+  return (
+    <div>
+      <h1>Globalise</h1>
+      <Design/>
+      <Document/>
+      <Search/>
+    </div>
+  );
+}
