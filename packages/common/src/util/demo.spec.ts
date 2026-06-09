078b1d346ad062f973bb21e5cfd060b93e10c8e9
Bas Leenknegt <basleenknegt@gmail.com>
2026-06-09 15:18:38 +0200

    Fix demo spec: remove test of removed util fn

--- packages/common/src/util/demo.spec.ts
+++ packages/common/src/util/demo.spec.ts
@@ -1,20 +1,5 @@
 import { describe, it, expect } from "vitest";
-import { tryOr } from "./tryOr";
 import { orThrow } from "./orThrow";
-
-describe("tryOr", () => {
-  it("returns the result of the function if it does not throw", () => {
-    const result = tryOr(() => "success", "fallback");
-    expect(result).toBe("success");
-  });
-
-  it("returns the default value if the function throws", () => {
-    const result = tryOr(() => {
-      throw new Error("fail");
-    }, "fallback");
-    expect(result).toBe("fallback");
-  });
-});
 
 describe("orThrow", () => {
   it("throws an error with the specified message", () => {
