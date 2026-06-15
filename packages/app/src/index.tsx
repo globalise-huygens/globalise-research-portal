896a1a0e8839a9fe87b8b770ec2e576800e9d129
Jona Schlegel <jona.schlegel@gmail.com>
2026-06-15 11:22:53 +0200

    Scope design globals changes

--- packages/app/src/index.tsx
+++ packages/app/src/index.tsx
@@ -1,11 +1,10 @@
-import '@globalise/design/globals.css';
-import { RouterProvider } from '@tanstack/react-router';
-import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { router } from './router.tsx';
+import { RouterProvider } from '@tanstack/react-router';
+import { StrictMode } from 'react';
 
 const root = document.getElementById('root');
-if (!root) {
+if(!root) {
   throw new Error('No root element');
 }
 createRoot(root).render(
