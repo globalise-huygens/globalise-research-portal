14246f1e932bded1cd10befa14685e2f00f09e44
Jona Schlegel <jona.schlegel@gmail.com>
2026-08-04 10:41:56 +0200

    Add portal navbar

--- packages/app/src/routes/__root.tsx
+++ packages/app/src/routes/__root.tsx
@@ -1,5 +1,15 @@
 import { createRootRoute, Outlet } from '@tanstack/react-router';
+import { SiteNavbar } from '../SiteNavbar.tsx';
 
 export const Route = createRootRoute({
-  component: () => <Outlet />,
+  component: RootLayout,
 });
+
+function RootLayout() {
+  return (
+    <>
+      <SiteNavbar />
+      <Outlet />
+    </>
+  );
+}
