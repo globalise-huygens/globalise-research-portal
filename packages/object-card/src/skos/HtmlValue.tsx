fb82e80b005558b31b08d8025d2c5b1d11d28cef
Jona Schlegel <jona.schlegel@gmail.com>
2026-08-07 15:33:28 +0200

    Add markdown styling

--- packages/object-card/src/skos/HtmlValue.tsx
+++ packages/object-card/src/skos/HtmlValue.tsx
@@ -7,31 +7,38 @@ type HtmlValueProps = {
 
 const URL_PATTERN = /(https?:\/\/[^\s<>"']*[^\s<>"'.,;:!?])/gi;
 
+function renderMarkdownLite(value: string): string {
+  return value
+    .replace(/(\*\*|__)([^\n]+?)\1/g, '<strong>$2</strong>')
+    .replace(/(^|[\s(])\*(?!\s)([^*\n]+?)\*(?!\*)/g, '$1<em>$2</em>')
+    .replace(/(^|[\s(])_(?!\s)([^_\n]+?)_(?!_)/g, '$1<em>$2</em>');
+}
+
 export function HtmlValue({ value }: HtmlValueProps) {
-  const parts = DOMPurify.sanitize(value).split(URL_PATTERN);
+  const htmlValue = renderMarkdownLite(value);
+  const parts = DOMPurify.sanitize(htmlValue).split(URL_PATTERN);
 
   return (
     <span className="concept-card__html-value">
-      {parts.map((part, index) => /^https?:\/\//i.test(part) ? (
-        <a
-          key={index}
-          href={part}
-          target="_blank"
-          rel="noopener noreferrer"
-          className="concept-card__inline-link"
-        >
-          <span className="concept-card__inline-link-label">{part}</span>
-          <IconExternalLink
-            aria-hidden="true"
-            className="concept-card__inline-link-icon"
-          />
-        </a>
-      ) : (
-        <span
-          key={index}
-          dangerouslySetInnerHTML={{ __html: part }}
-        />
-      ))}
+      {parts.map((part, index) =>
+        /^https?:\/\//i.test(part) ? (
+          <a
+            key={index}
+            href={part}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="concept-card__inline-link"
+          >
+            <span className="concept-card__inline-link-label">{part}</span>
+            <IconExternalLink
+              aria-hidden="true"
+              className="concept-card__inline-link-icon"
+            />
+          </a>
+        ) : (
+          <span key={index} dangerouslySetInnerHTML={{ __html: part }} />
+        ),
+      )}
     </span>
   );
 }
