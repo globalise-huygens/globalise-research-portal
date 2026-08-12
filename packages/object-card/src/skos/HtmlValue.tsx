28c63b8a798a905a0e5e6f5037eb9bb115465559
Jona Schlegel <jona.schlegel@gmail.com>
2026-08-12 15:51:43 +0200

    Move Markdown rendering out of the concept card PR

--- packages/object-card/src/skos/HtmlValue.tsx
+++ packages/object-card/src/skos/HtmlValue.tsx
@@ -1,26 +1,13 @@
 import DOMPurify from 'dompurify';
-import MarkdownIt from 'markdown-it';
-
-const markdown = new MarkdownIt('zero', {
-  html: false,
-  breaks: true,
-  linkify: false,
-});
-
-markdown.enable(['emphasis', 'newline']);
 
 type HtmlValueProps = {
-  value: string;
+  value: string
 };
 
-export function renderMarkdown(value: string): string {
-  const html = markdown.renderInline(value);
-  return DOMPurify.sanitize(html, {
-    ALLOWED_TAGS: ['em', 'br'],
-    ALLOWED_ATTR: [],
-  });
-}
-
 export function HtmlValue({ value }: HtmlValueProps) {
-  return <span dangerouslySetInnerHTML={{ __html: renderMarkdown(value) }} />;
+  const sanitized = DOMPurify.sanitize(value);
+  return <span
+    dangerouslySetInnerHTML={{ __html: sanitized }}
+    style={{ whiteSpace: 'pre-line' }}
+  ></span>;
 }
