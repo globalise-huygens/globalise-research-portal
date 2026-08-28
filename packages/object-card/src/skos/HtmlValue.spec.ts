1d99787757c72831a5a1b59af463155349296771
Jona Schlegel <jona.schlegel@gmail.com>
2026-08-28 13:47:48 +0200

    Limit concept Markdown rendering

--- packages/object-card/src/skos/HtmlValue.spec.ts
+++ packages/object-card/src/skos/HtmlValue.spec.ts
@@ -5,16 +5,10 @@ describe(renderMarkdown.name, () => {
   it.each([
     ['*Omhouwer*', '<em>Omhouwer</em>'],
     ['First\nSecond', 'First<br>\nSecond'],
-    [
-      '[Getty AAT](https://www.getty.edu/)',
-      '<a href="https://www.getty.edu/">Getty AAT</a>',
-    ],
-    [
-      'https://www.zotero.org/item',
-      '<a href="https://www.zotero.org/item">https://www.zotero.org/item</a>',
-    ],
+    ['Plain text', 'Plain text'],
+    ['[Getty AAT](https://www.getty.edu/)', '[Getty AAT](https://www.getty.edu/)'],
+    ['https://www.zotero.org/item', 'https://www.zotero.org/item'],
     ['**Bold** <strong>HTML</strong>', 'Bold &lt;strong&gt;HTML&lt;/strong&gt;'],
-    ['[Link](javascript:alert(1))', '[Link](javascript:alert(1))'],
   ])('renders %s safely', (markdown, html) => {
     expect(renderMarkdown(markdown)).toBe(html);
   });
